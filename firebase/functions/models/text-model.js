dcb9ba6d3b60fecf8e555114c09da935d81e7a54
Suélinton Farias <suelinton.farias@gmail.com>
2019-11-12 17:46:35 -0300

    Correção.

---
+++ firebase/functions/models/text-model.js
@@ -0,0 +1,12 @@
+function getTextModel(text){
+    return {
+        "text": {
+            "text": [
+                text
+            ]
+        },
+        "platform": "FACEBOOK"
+    }
+}
+
+module.exports = getTextModel;
